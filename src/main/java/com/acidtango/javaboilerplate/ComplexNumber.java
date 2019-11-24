f0f1e9d78b064f36e5f5c2d5c77146bbf62bb0cc
Daniel Ramos <danielramosacosta@hotmail.com>
2019-11-24 16:23:39 +0100

    Add toString and subtract method

--- src/main/java/com/acidtango/javaboilerplate/ComplexNumber.java
+++ src/main/java/com/acidtango/javaboilerplate/ComplexNumber.java
@@ -1,21 +1,22 @@
 package com.acidtango.javaboilerplate;
 
 
-import java.util.Arrays;
-import java.util.List;
 import java.util.Objects;
-import java.util.stream.Collectors;
 
 public class ComplexNumber {
-    private static final String BEGIN_CUSTOM_SEPARATOR_TOKEN = "//";
-    private static final String END_CUSTOM_SEPARATOR_TOKEN = ";";
-    private static final String DEFAULT_SEPARATOR_TOKEN = ",";
     private final Integer realPart;
     private final Integer complexPart;
 
     public ComplexNumber(Integer realPart, Integer complexPart) {
         this.realPart = realPart;
         this.complexPart = complexPart;
+    }
+
+    @Override
+    public String toString() {
+        String middleSymbol = complexPart < 0 ? "-" : "+";
+
+        return String.format("%d %s %di", realPart, middleSymbol, Math.abs(complexPart));
     }
 
     @Override
@@ -32,53 +33,12 @@ public class ComplexNumber {
         return Objects.hash(realPart, complexPart);
     }
 
-    public static int calculate(String numberString) {
-        String separatorToken = getSeparatorToken(numberString);
-        String numberStringWithoutSeparator = getNumberStringWithoutSeparator(numberString);
-
-        List<Integer> numbers = parseNumberString(numberStringWithoutSeparator, separatorToken);
-
-        return numbers.stream().mapToInt(t -> t).sum();
-    }
-
-    private static String getNumberStringWithoutSeparator(String numberString) {
-        if (hasCustomSeparator(numberString)) {
-            Integer indexOfStartNumberStringWithoutSeparator = getIndexOfStartNumberStringWithoutSeparator(numberString);
-            return numberString.substring(indexOfStartNumberStringWithoutSeparator);
-        }
-        return numberString;
-    }
-
-    private static Integer getIndexOfStartNumberStringWithoutSeparator(String numberString) {
-        return getIndexOfEndCustomSeparator(numberString) + 1;
-    }
-
-    private static String getSeparatorToken(String numberString) {
-        if (hasCustomSeparator(numberString)) {
-            return numberString.substring(
-                BEGIN_CUSTOM_SEPARATOR_TOKEN.length(),
-                getIndexOfEndCustomSeparator(numberString)
-            );
-        }
-        return DEFAULT_SEPARATOR_TOKEN;
-    }
-
-    private static boolean hasCustomSeparator(String numberString) {
-        return numberString.startsWith(BEGIN_CUSTOM_SEPARATOR_TOKEN);
-    }
-
-    private static Integer getIndexOfEndCustomSeparator(String numberString) {
-        return numberString.indexOf(END_CUSTOM_SEPARATOR_TOKEN);
-    }
-
-
-    private static List<Integer> parseNumberString(String numberString, String separatorToken) {
-        return Arrays.stream(numberString.split(separatorToken))
-                .map(Integer::parseInt)
-                .collect(Collectors.toList());
-    }
 
     public ComplexNumber add(ComplexNumber b) {
-        return new ComplexNumber(this.realPart + b.realPart, this.complexPart + b.complexPart);
+        return new ComplexNumber(realPart + b.realPart, complexPart + b.complexPart);
+    }
+
+    public ComplexNumber subtract(ComplexNumber b) {
+        return new ComplexNumber(realPart - b.realPart, complexPart - b.complexPart);
     }
 }
--- src/test/java/unit/ComplexNumberShould.java
+++ src/test/java/unit/ComplexNumberShould.java
@@ -7,6 +7,13 @@ import org.junit.jupiter.api.Test;
 
 class ComplexNumberShould {
     @Test
+    void be_convertible_to_string() {
+        assertEquals("1 + 2i", new ComplexNumber(1, 2).toString());
+        assertEquals("1 - 2i", new ComplexNumber(1, -2).toString());
+        assertEquals("-1 - 2i", new ComplexNumber(-1, -2).toString());
+    }
+
+    @Test
     void be_comparable_to_other_complex() {
         assertEquals(new ComplexNumber(1, 1), new ComplexNumber(1, 1));
     }
@@ -18,6 +25,16 @@ class ComplexNumberShould {
 
         ComplexNumber c = a.add(b);
 
-        assertEquals(c, new ComplexNumber(3, 3));
+        assertEquals(new ComplexNumber(3, 3), c);
+    }
+
+    @Test
+    void be_subtracted_to_other_complex_number() {
+        ComplexNumber a = new ComplexNumber(1, 1);
+        ComplexNumber b = new ComplexNumber(2, 2);
+
+        ComplexNumber c = a.subtract(b);
+
+        assertEquals(c, new ComplexNumber(-1, -1));
     }
 }
